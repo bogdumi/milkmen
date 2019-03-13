158d54d98787f6c3c4b92d7aef9d108001644358
Cassandra McCormack <cassandra.tablet55@gmail.com>
2019-03-13 14:29:05 +0000

    added movement

--- src/main/java/com/contestantbots/bots/ThanOS.java
+++ src/main/java/com/contestantbots/bots/ThanOS.java
@@ -3,15 +3,21 @@ package com.contestantbots.bots;
 import com.contestantbots.framework.Commander;
 import com.contestantbots.framework.ContestantBotImplementation;
 import com.contestantbots.framework.GameState;
+import com.contestantbots.framework.*;
+import com.scottlogic.hackathon.game.*;
 
 public class ThanOS extends ContestantBotImplementation {
 
     
 
     public ThanOS() { super("ThanOS"); }
-    
+
 
     public void getReady(GameState gameState) {}
 
-    public void issueCommands(GameState gameState, Commander commander) {}
+    public void issueCommands(GameState gameState, Commander commander) {
+        for (Unit unit : gameState.getMyUnits()) {
+            commander.issueMoveOrder(unit, Direction.NORTH);
+        }
+    }
 }
