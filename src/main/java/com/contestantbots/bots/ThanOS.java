beb48c49596fec9b09ecbe146ea3e8096dd3b5d8
Bogdan Dumitrescu <bogdoomy@gmail.com>
2019-03-13 14:40:15 +0000

    Start A*

--- src/main/java/com/contestantbots/bots/ThanOS.java
+++ src/main/java/com/contestantbots/bots/ThanOS.java
@@ -6,8 +6,22 @@ import com.contestantbots.framework.GameState;
 
 public class ThanOS extends ContestantBotImplementation {
 
-    public star(int targetX, int targetY){
-        Node openList[], closedList;
+    public star(int currentX, int currentY, int targetX, int targetY){
+        List openList = new ArrayList();
+        List closedList = new ArrayList();
+        Node startNode = new Node(currentX, currentY);
+
+        openList.add(startNode);
+
+        while(!openList.isEmpty()){
+            Node q = new Node(0,0);
+            q.setF(100000);
+            for(Node nd : openList){
+                if(nd.getF < q.getF)
+                    q = nd;
+            }
+            
+        }
     }
 
     public ThanOS() { super("ThanOS"); }
--- src/main/java/com/contestantbots/framework/Node.java
+++ src/main/java/com/contestantbots/framework/Node.java
@@ -7,5 +7,43 @@ public class Node {
     public Node (int x, int y){
         this.x = x;
         this.y = y;
+        this.f = 0;
     }
+
+    public calcF(){
+
+    }
+
+    public calcG(){
+
+    }
+
+    public calcH(){
+
+    }
+
+    public void setF(int f){
+        this.f = f;
+    }
+
+    public void setG(int g){
+        this.g = g;
+    }
+
+    public void setH(int h){
+        this.h = h;
+    }
+
+    public int getF(){
+        return this.f;
+    }
+
+    public int getG(){
+        return this.g;
+    }
+
+    public int getH(){
+        return this.h;
+    }
+
 }
