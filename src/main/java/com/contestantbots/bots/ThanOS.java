804e8dbea835ca9c5b7c74bfa8e287eef5d28963
Bogdan Dumitrescu <bogdoomy@gmail.com>
2019-03-13 16:12:52 +0000

    not working but hopeful

--- src/main/java/com/contestantbots/bots/ThanOS.java
+++ src/main/java/com/contestantbots/bots/ThanOS.java
@@ -3,30 +3,53 @@ package com.contestantbots.bots;
 import com.contestantbots.framework.Commander;
 import com.contestantbots.framework.ContestantBotImplementation;
 import com.contestantbots.framework.GameState;
+import com.contestantbots.framework.Node;
+import com.scottlogic.hackathon.game.Direction;
+import com.scottlogic.hackathon.game.Position;
+import java.util.List;
+import java.lang.Object;
+import java.util.ArrayList;
+
+int f-matrix[100][100];
 
 public class ThanOS extends ContestantBotImplementation {
 
-    public star(int currentX, int currentY, int targetX, int targetY){
-        List openList = new ArrayList();
-        List closedList = new ArrayList();
-        Node startNode = new Node(currentX, currentY);
+    public void star(Position currentPos, Position targetPos){
+        List<Node> openList = new ArrayList();
+        List<Node> closedList = new ArrayList();
+        Node startNode = new Node();
+        startNode.setPos(currentPos);
 
         openList.add(startNode);
 
         while(!openList.isEmpty()){
-            Node q = new Node(0,0);
+
+            Node q = new Node();
             q.setF(100000);
+            
             for(Node nd : openList){
-                if(nd.getF < q.getF)
+                if(nd.getF() < q.getF())
                     q = nd;
             }
-            
+
+            List<Node> succ = q.genSucc();
+
+            for(Node nd : succ){
+                if(nd.getPos() == targetPos)
+                    return;
+                nd.calcF(targetPos);
+                
+            }
         }
     }
 
     public ThanOS() { super("ThanOS"); }
 
-    public void getReady(GameState gameState) {}
+    public void getReady(GameState gameState) {
 
-    public void issueCommands(GameState gameState, Commander commander) {}
+    }
+
+    public void issueCommands(GameState gameState, Commander commander) {
+        
+    }
 }
--- src/main/java/com/contestantbots/framework/Node.java
+++ src/main/java/com/contestantbots/framework/Node.java
@@ -1,25 +1,90 @@
 package com.contestantbots.framework;
+import com.contestantbots.framework.Commander;
+import com.contestantbots.framework.ContestantBotImplementation;
+import com.contestantbots.framework.GameState;
+import com.scottlogic.hackathon.game.Position;
+import java.lang.*;
+import java.util.List;
+import java.lang.Object;
+import java.util.ArrayList;
 
 public class Node {
-    int x,y;
+    Position pos;
+    Node parent;
     int f,g,h;
 
-    public Node (int x, int y){
-        this.x = x;
-        this.y = y;
+    public Node (){
         this.f = 0;
     }
 
-    public calcF(){
+    public List<Node> genSucc(){
+        List<Node> res = new ArrayList();
+        Node aux = new Node();
+        aux.setParent(this);
 
+        Position auxpos1 = new Position(this.getPos().getX()-1, this.getPos().getY()-1);
+        aux.setPos(auxpos1);
+        res.add(aux);
+
+        Position auxpos2 = new Position(this.getPos().getX()-1, this.getPos().getY());
+        aux.setPos(auxpos2);
+        res.add(aux);
+
+        Position auxpos3 = new Position(this.getPos().getX()-1, this.getPos().getY()+1);
+        aux.setPos(auxpos3);
+        res.add(aux);
+
+        Position auxpos4 = new Position(this.getPos().getX(), this.getPos().getY()-1);
+        aux.setPos(auxpos4);
+        res.add(aux);
+
+        Position auxpos5 = new Position(this.getPos().getX(), this.getPos().getY()+1);
+        aux.setPos(auxpos5);
+        res.add(aux);
+
+        Position auxpos6 = new Position(this.getPos().getX()+1, this.getPos().getY()-1);
+        aux.setPos(auxpos6);
+        res.add(aux);
+
+        Position auxpos7 = new Position(this.getPos().getX()+1, this.getPos().getY());
+        aux.setPos(auxpos7);
+        res.add(aux);
+
+        Position auxpos8 = new Position(this.getPos().getX()+1, this.getPos().getY()+1);
+        aux.setPos(auxpos8);
+        res.add(aux);
+
+        return res;
     }
 
-    public calcG(){
-
+    public void setPos(Position pos){
+        this.pos = pos;
     }
 
-    public calcH(){
+    public Position getPos(){
+        return this.pos;
+    }
 
+    public void setParent(Node nd){
+        this.parent = nd;
+    }
+
+    public Node getParent(){
+        return this.parent;
+    }
+
+    public void calcF(Position target){
+        this.calcG();
+        this.calcH(target);
+        this.f = this.g + this.h;
+    }
+
+    public void calcG(){
+        this.g = this.parent.g + 1;
+    }
+
+    public void calcH(Position target){
+        this.h = Math.max(Math.abs(this.pos.getX() - target.getX()), Math.abs(this.pos.getY() - target.getY()));
     }
 
     public void setF(int f){
